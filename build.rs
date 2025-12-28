5526d605ca7d8fc37f3295e757355f3733dbaacc
Sakurine <yichengxin7@gmail.com>
2025-12-28 14:16:47 +0800

    删除 build.rs 文件，移除 Windows 图标资源设置

--- build.rs
+++
@@ -1,8 +0,0 @@
-// build.rs
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("avatar-modified.ico");
-        res.compile().unwrap();
-    }
-}
